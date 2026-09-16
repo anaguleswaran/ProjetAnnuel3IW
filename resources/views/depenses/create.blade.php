9ecdffda86ad6b5ffbb7ae5c85d7019ad439f3ac
anaguleswaran <alicia.naguleswaran@outlook.com>
2026-09-16 18:28:13 +0200

    ajouter le filtre de recherche des dépenses (closes #17)

--- resources/views/depenses/create.blade.php
+++ resources/views/depenses/create.blade.php
@@ -92,6 +92,15 @@
                         toggleFields();
                     </script>
                 </div>
+                @if ($errors->any())
+                    <div style="color: red; margin-bottom: 20px;">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
